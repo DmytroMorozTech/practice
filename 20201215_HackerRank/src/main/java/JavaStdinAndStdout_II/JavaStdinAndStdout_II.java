ea7a1c79673726f0c775e959ca4745d54a2d3927
Dmytro Moroz <dmytro.moroz3@gmail.com>
2020-12-27 12:57:35 +0200

    Standard input in Java

---
+++ 20201215_HackerRank/src/main/java/JavaStdinAndStdout_II/JavaStdinAndStdout_II.java
@@ -0,0 +1,40 @@
+package JavaStdinAndStdout_II;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout_II {
+
+    public static class Solution {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int i = scan.nextInt();
+            scan.nextLine(); // Pay attention to that we should always consume the buffered
+            // left-over (in this case it was "\n"). Read below for more.
+            double d = scan.nextDouble();
+            scan.nextLine();
+            String s = scan.nextLine();
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+        }
+    }
+}
+
+//            https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
+//            That's because the Scanner.nextInt method does not read the newline character in your
+//            input created by hitting "Enter," and so the call to Scanner.nextLine returns after
+//            reading that newline.
+//
+//            You will encounter the similar behaviour when you use Scanner.nextLine after
+//            Scanner.next() or any Scanner.nextFoo method (except nextLine itself).
+//
+//            Workaround:
+//
+//            Either put a Scanner.nextLine call after each Scanner.nextInt or Scanner.nextFoo
+//             to consume rest of that line including newline
+//
+//            int option = input.nextInt();
+//            input.nextLine();  // Consume newline left-over  !!!
+//            String str1 = input.nextLine();
