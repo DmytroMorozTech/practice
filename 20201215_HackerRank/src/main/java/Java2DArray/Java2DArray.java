3086c67969b55ae3d49db189b0232645981669b0
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-11-16 15:38:40 +0200

    Java 2D array_MaxHourglassSum task

--- 20201215_HackerRank/src/main/java/Java2DArray/Java2DArray.java
+++ 20201215_HackerRank/src/main/java/Java2DArray/Java2DArray.java
@@ -1,57 +1,56 @@
 //https://www.hackerrank.com/challenges/java-2d-array/problem
 
-// Article that turned out to be handy (re: Character.getNumericValue(char))
-//https://stackoverflow.com/questions/46343616/how-can-i-convert-a-char-to-int-in-java/46343730
 package Java2DArray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Java2DArray {
     public static void main(String[] args) {
-        List<String> strings = new ArrayList<>(
+        List<List<Integer>> srcData1 = new ArrayList<>(
                 Arrays.asList(
-                        "1 1 1 0 0 0",
-                        "0 1 0 0 0 0",
-                        "1 1 1 0 0 0",
-                        "0 0 2 4 4 0",
-                        "0 0 0 2 0 0",
-                        "0 0 1 2 4 0"
-                ));
+                        Arrays.asList(1, 1, 1, 0, 0, 0),
+                        Arrays.asList(0, 1, 0, 0, 0, 0),
+                        Arrays.asList(1, 1, 1, 0, 0, 0),
+                        Arrays.asList(0, 0, 2, 4, 4, 0),
+                        Arrays.asList(0, 0, 0, 2, 0, 0),
+                        Arrays.asList(0, 0, 1, 2, 4, 0))
+        );
 
-        List<String> stringsTrimmed = strings
-                .stream()
-                .map(s -> s.replace(" ", ""))
-                .collect(Collectors.toList());
+        List<List<Integer>> srcData2 = new ArrayList<>(
+                Arrays.asList(
+                        Arrays.asList(-9, -9, -9, 1, 1, 1),
+                        Arrays.asList(0, -9, 0, 4, 3, 2),
+                        Arrays.asList(-9, -9, -9, 1, 2, 3),
+                        Arrays.asList(0, 0, 8, 6, 6, 0),
+                        Arrays.asList(0, 0, 0, -2, 0, 0),
+                        Arrays.asList(0, 0, 1, 2, 4, 0))
+        );
 
-        int h = 6;
-        int w = 6;
-        int[][] data = new int[h][w];
+        System.out.println("MaxHourglassSum in 2D array (DATASET No.1): ");
+        System.out.println(calculateMaxHourglassSum(srcData1));
 
-        IntStream.range(0, h).forEach(y ->
-                IntStream.range(0, w).forEach(x -> {
-                    data[y][x] = Character.getNumericValue(stringsTrimmed.get(y).charAt(x));
-                }));
-
-        List<Integer> sumsOfHourglasses = new ArrayList<>();
-        for (int y = 0; y <= 3; y++) {
-            for (int x = 0; x <= 3; x++) {
-                sumsOfHourglasses.add(calculateHourglassSum(data, y, x));
-            }
-        }
-
-        Integer max = Collections.max(sumsOfHourglasses);
-        System.out.println(max);
+        System.out.println("MaxHourglassSum in 2D array (DATASET No.2): ");
+        System.out.println(calculateMaxHourglassSum(srcData2));
     }
 
 
-    public static int calculateHourglassSum(int[][] data, int y, int x) {
-        return data[y][x] + data[y][x + 1] + data[y][x + 2]
-                + data[y + 1][x + 1]
-                + data[y + 2][x] + data[y + 2][x + 1] + +data[y + 2][x + 2];
+    public static int calculateMaxHourglassSum(List<List<Integer>> input) {
+        List<Integer> sumsOfHourglasses = new ArrayList<>();
+        for (int y = 0; y <= 3; y++) {
+            for (int x = 0; x <= 3; x++) {
+                sumsOfHourglasses.add(calculateHourglassSum(input, y, x));
+            }
+        }
+
+        return Collections.max(sumsOfHourglasses);
+    }
+
+    public static int calculateHourglassSum(List<List<Integer>> data, int y, int x) {
+        return data.get(y).get(x) + data.get(y).get(x + 1) + data.get(y).get(x + 2)
+                + data.get(y + 1).get(x + 1)
+                + data.get(y + 2).get(x) + data.get(y + 2).get(x + 1) + data.get(y + 2).get(x + 2);
     }
 }
